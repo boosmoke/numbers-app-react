f4aa1926c233c0feab7aadbdd3449610ccffd0a0
Ben <benjah88@gmail.com>
2020-05-05 16:07:00 -0700

    ben test

--- src/components/Example/index.js
+++ src/components/Example/index.js
@@ -7,12 +7,59 @@ class Example extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
+      win: ""
      };
+  }
+
+  shuffle(array) {
+    if(!array) {
+      array = numbers;
+    }
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+  checkIfWin = () => {
+    let isWinner = false;
+    numbers.sort((a, b) => {
+      if(a>b) {
+        if(a === numbers[numbers.length - 1]){
+          isWinner =  true;
+        }
+      }
+    });
+    if(!isWinner){
+       return false;
+   } else {
+    this.setState({win: "YOU WON! CONGRATS!!!"});
+    }
   }
 
   moveBox = (number) => {
     let thisy = document.getElementById(number);
     let empty = document.getElementById("empty-box");
+
+ let swap = () => {
+      let texty = thisy.textContent;
+      thisy.id = "empty-box";
+      thisy.textContent = " ";
+      empty.id = texty;
+      empty.textContent = texty;
+      this.checkIfWin();
+    }
 
     /*
     console.log("THIS LEFT", thisy.offsetLeft);
@@ -50,6 +97,7 @@ class Example extends React.Component {
           empty.id = texty;
           empty.textContent = texty;
       }, 500);
+      
     }
 
     if (thisy.offsetLeft === empty.offsetLeft && thisy.offsetTop === empty.offsetTop + 174)
@@ -81,6 +129,7 @@ class Example extends React.Component {
           empty.id = texty;
           empty.textContent = texty;
       }, 500);
+      this.checkIfWin();
     }
 
     if (thisy.offsetTop === empty.offsetTop && thisy.offsetLeft === (empty.offsetLeft - 270))
@@ -93,7 +142,8 @@ class Example extends React.Component {
         // timing options
         duration: 500,
         iterations: 1
-      });
+      },
+       swap());
 
       empty.animate([
         // keyframes
@@ -103,7 +153,8 @@ class Example extends React.Component {
         // timing options
         duration: 500,
         iterations: 1
-      });
+      },
+      swap());
 
     
     setTimeout(function(){
@@ -113,6 +164,7 @@ class Example extends React.Component {
       empty.id = texty;
       empty.textContent = texty;
     }, 500);
+    this.checkIfWin();
     }
 
     if (thisy.offsetTop === empty.offsetTop && thisy.offsetLeft === (empty.offsetLeft + 270))
@@ -145,13 +197,21 @@ class Example extends React.Component {
       empty.id = texty;
       empty.textContent = texty;
     }, 500);
-    }
+    this.checkIfWin();
   }
+  }
+
+  componentDidMount () {
+    this.shuffle(numbers);
+  }
+
   render() {
     return(
       <div>
         <h1>Numbers Game</h1>
         <div className="container">
+        <h2>{this.state.win}</h2>
+        <p class="box reset" onClick={() => this.shuffle(numbers)}>RESET</p>
           <div className="row">
             {numbers.map((number) => {   
               return (Number.isInteger(number) ? <div onClick={() => this.moveBox(number)} id={number} className="box">{number}</div> : <div id="empty-box" className="box">{number}</div>)

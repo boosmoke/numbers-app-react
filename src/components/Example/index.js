91296f09688e0173cfe71bbc4c04a2e8765c5b48
Ben <benjah88@gmail.com>
2020-05-05 13:06:16 -0700

    box setup

--- src/components/Example/index.js
+++ src/components/Example/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './styles.css'
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
 const Example = ({}) =>
     <div>
-      <h1>Example component</h1>
+      <h1>Numbers Game</h1>
+      <div className="container">
+        <div className="row">
+        {numbers.map((number) => {   
+           return (<div className="box">{number}</div>) 
+        })}
+        <div className="box empty"></div>
+        </div>
+      </div>
     </div>;
 
 export default Example

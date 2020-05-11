271090434bb54f72a4670baf3d3e48cf74938976
boosmoke <lybeck.john@gmail.com>
2020-05-11 14:45:10 -0700

    Updates

---
+++ precache-manifest.704cc53ceb9ba4077d7a7153fa7d655e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b1ce0eb4f89ec9bb82ad256474c3dc",
+    "url": "/numbers-app-react/index.html"
+  },
+  {
+    "revision": "568e13a7c02379ef300a",
+    "url": "/numbers-app-react/static/css/main.2c780acd.chunk.css"
+  },
+  {
+    "revision": "596df6f3cb2d4c7ac752",
+    "url": "/numbers-app-react/static/js/2.a208b627.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/numbers-app-react/static/js/2.a208b627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568e13a7c02379ef300a",
+    "url": "/numbers-app-react/static/js/main.505046ad.chunk.js"
+  },
+  {
+    "revision": "6d4fa97873ed4ce1ccd6",
+    "url": "/numbers-app-react/static/js/runtime-main.5adcbab4.js"
+  }
+]);
